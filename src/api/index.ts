30d8ec6d7899dd6f4567b836a0ab1bfc9094679e
sofyannurrohman <sofyannurrohman1@gmail.com>
2025-06-24 11:13:15 +0700

    add log

--- src/api/index.ts
+++ src/api/index.ts
@@ -3,6 +3,7 @@ import axios from 'axios';
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL,
 });
+console.log("Using API Base URL:", import.meta.env.VITE_API_BASE_URL)
 
 api.interceptors.request.use(config => {
   const token = localStorage.getItem('token');
