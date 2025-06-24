854490793c00839632853fe6b736121075ec73cf
sofyannurrohman <sofyannurrohman1@gmail.com>
2025-06-24 12:10:24 +0700

    force https

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,10 +1,14 @@
 import axios from 'axios';
 
-const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL,
-});
-console.log("Using API Base URL:", import.meta.env.VITE_API_BASE_URL)
+const baseURL = import.meta.env.VITE_API_BASE_URL.startsWith('http')
+  ? import.meta.env.VITE_API_BASE_URL.replace('http://', 'https://')
+  : import.meta.env.VITE_API_BASE_URL;
 
+console.log("Using API Base URL:", baseURL);
+
+const api = axios.create({
+  baseURL,
+});
 api.interceptors.request.use(config => {
   const token = localStorage.getItem('token');
   if (token) {
